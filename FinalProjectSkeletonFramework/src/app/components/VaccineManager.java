57c46b6fc7b9675687cd2a6be83bc68cc2aaa2b6
abes26 <95269002+abes26@users.noreply.github.com>
2021-12-12 10:48:45 +0800

    Update VaccineManager.java

--- FinalProjectSkeletonFramework/src/app/components/VaccineManager.java
+++ FinalProjectSkeletonFramework/src/app/components/VaccineManager.java
@@ -9,29 +9,38 @@ import app.entity.Vaccine;
 @Component
 public class VaccineManager {
 
-	public String createVaccine(String name, String manufacturer) {
+	@Autowired
+	VaccineRepository vaccineRepo;
+	
+	public String createVaccine(String name) {
+		Vaccine newVaccine = new Vaccine();
+		newVaccine.setName(name);
+		newVaccine.setManufacturer("");
+		newVaccine = vaccineRepo.save(newVaccine);
 		return "Vaccine Created!";
 	}
 
 	public String deleteVaccine(Long id) {
-		// TODO Auto-generated method stub
+		vaccineRepo.deleteById(id);
 		return "Vaccine Deleted!";
 	}
 
-	public String viewVaccineDetails(Long id) {
-		// TODO Auto-generated method stub
-		return "Vaccine Details:";
+	public Optional<Vaccine> viewVaccineDetials(Long id)
+	{
+		Optional<Vaccine> vaccineDetails = vaccineRepo.findById(id);
+		return vaccineDetails;
 	}
 
 	public List<Vaccine> viewAllVaccines() {
-		return null;
+		return vaccineRepo.findAll();
 	}
 
 	public String updateVaccine(Long id, String name, String manufacturer) {
-		// TODO Auto-generated method stub
+		Vaccine updateVaccine = vaccineRepo.getById(id);
+		updateVaccine.setName(name);
+		vaccineRepo.save(updateVaccine);
 		return "Vaccine Updated!";
 	}
-
 
 
 }
