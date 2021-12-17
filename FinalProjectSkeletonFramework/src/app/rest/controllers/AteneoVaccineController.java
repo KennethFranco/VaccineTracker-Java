ceb17b23424bc43eb3fe71cf10faf4afae0104ff
Marco Francisco <franciscomarco2921@gmail.com>
2021-12-17 14:12:32 +0800

    Removed Add/Remove Student to Department

--- FinalProjectSkeletonFramework/src/app/rest/controllers/AteneoVaccineController.java
+++ FinalProjectSkeletonFramework/src/app/rest/controllers/AteneoVaccineController.java
@@ -337,24 +337,6 @@ public class AteneoVaccineController {
 	}
 	
 	@POST
-	@Path("/addStudentToDepartment")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-	public String addStudentToDepartment(@FormParam("studentID") Long studentID, @FormParam("departmentID") Long departmentID) 
-	{
-		return dm.addStudentToDepartment(studentID, departmentID);
-	}
-	
-	@POST
-	@Path("/removeStudentFromDepartment")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-	public String removeStudentFromDepartment(@FormParam("studentID") Long studentID, @FormParam("departmentID") Long departmentID) 
-	{
-		return dm.removeStudentFromDepartment(studentID, departmentID);
-	}
-	
-	@POST
 	@Path("/addFacultyToDepartment")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
